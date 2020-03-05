58a136571f6c7f62bbda8030ccdac04a949f5b2b
MastaAaron <a.resing@hotmail.com>
2020-03-05 08:42:31 -0600

    fix test to use correct function

--- src/test.js
+++ src/test.js
@@ -4,5 +4,5 @@ import { sum, sumDelayed } from './index'
 test(`Adds numbers together`, async () => {
   expect(sum(1, 2, 3)).toBe(6)
   expect(sum(5, 5, 5, 6)).toBe(21)
-  expect(await sum(70, 30)).toBe(100)
+  expect(await sumDelayed(70, 30)).toBe(100)
 })
