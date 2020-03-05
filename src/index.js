3e57c22d9255b60bacb591b08be88af6eae4d7fe
MastaAaron <a.resing@hotmail.com>
2020-03-05 08:27:53 -0600

    add regenerator-runtime and core-js, add async tests for Babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 
 /**
- * Adds numbers together
+ * Adds numbers together (bogus async for testing purposes)
  */
 export function sum(...nums) {
   return nums.reduce((acc, curr) => acc + curr, 0)
+}
+
+
+/**
+ * Delayed version of above; returns promise for testing Babel
+ */
+export function sumDelayed(...nums) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(sum(...nums)), 1000)
+  })
 }
--- src/test.js
+++ src/test.js
@@ -1,7 +1,8 @@
-import { sum } from './index'
+import { sum, sumDelayed } from './index'
 
 
-test(`Adds numbers together`, () => {
+test(`Adds numbers together`, async () => {
   expect(sum(1, 2, 3)).toBe(6)
   expect(sum(5, 5, 5, 6)).toBe(21)
+  expect(await sum(70, 30)).toBe(100)
 })
